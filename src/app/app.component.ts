1cdfcf082f970c37f97a39124d441b6a8d0868d4
Gourav Nayak <gourav22nayak@gmail.com>
2022-07-02 20:04:01 -0400

    Initial code commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+    length = 0;
+    onChangeLength(value: string) {
+        const parsedValue = parseInt(value);
+        if (!isNaN(parsedValue) && parsedValue>0) {
+            this.length = parsedValue;
+        }
+    }
+
+    includeLetters = false;
+    onChangeUseLetters() {
+        this.includeLetters = !this.includeLetters;
+    }
+
+    includeNumbers = false;
+    onChangeUseNumbers() {
+        this.includeNumbers = !this.includeNumbers;
+    }
+
+    includeSymbols = false;
+    onChangeUseSymbols() {
+        this.includeSymbols = !this.includeSymbols;
+    }
+    password = '';
+
+    onGenerateButtonCLick() {
+        const numbers = '1234567890';
+        const letters = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+        const symbols = '!@#$%^&*()';
+
+        let validChars = '';
+
+        if (this.includeLetters) {
+            validChars += letters;
+        }
+        if (this.includeNumbers) {
+            validChars += numbers;
+        }
+        if (this.includeSymbols) {
+            validChars += symbols;
+        }
+        console.log(`About to generate a new password with following condition:
+            Include Letters: ${ this.includeLetters }
+            Include Numbers: ${this.includeNumbers}
+            Include Symbols: ${this.includeSymbols}
+            `);
+
+        let generatedpassword = '';
+
+        for (let i = 0; i < this.length; i++) {
+            const index = Math.floor(Math.random() * validChars.length);
+            generatedpassword += validChars[index];
+        }
+        this.password = generatedpassword;
+    }
+
 }
